564fa737982929fdc1feeb3f3988f1e2032f73e9
Asif Alam <104073769+Looperrrrrr@users.noreply.github.com>
2025-12-03 17:48:56 +0000

    swipe update

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -3,6 +3,10 @@ import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 import { Jumbotron } from "../components/Jumbotron.jsx";
 import Card from "../components/Card.jsx";
 import { Instrucciones } from "../components/Instrucciones.jsx";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/autoplay";
+import { Autoplay } from "swiper/modules";
 
 const imageUrl1 = "src/front/assets/img/regalo.jpg";
 const button = "¡Ver producto!";
@@ -45,37 +49,58 @@ export const Home = () => {
 
 			<Instrucciones />
 
-			<div className='ms-5 me-5 mb-5 d-flex row gap-4 justify-content-center'>
-				<h3 className="d-flex justify-content-center fw-bold mt-4">Otros usuarios han seleccionado estos regalos como favorito...</h3>
-				<Card
-					imageUrl={imageUrl1}
-					title="Título imagen"
-					description="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-					button={button} />
+			<Swiper
+				modules={[Autoplay]}
+				autoplay={{ delay: 2000, disableOnInteraction: false }}
+				spaceBetween={20}
+				slidesPerView={1}
+				loop={true}
+				breakpoints={{
+					768: { slidesPerView: 2 },
+					1024: { slidesPerView: 3 }
+				}}
+				className="mySwiper"
+			>
 
-				<Card
-					imageUrl={imageUrl1}
-					title="Título imagen"
-					description="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-					button={button} />
+				<div className='ms-5 me-5 mb-5 d-flex row gap-4 justify-content-center'>
 
-				<Card
-					imageUrl={imageUrl1}
-					title="Título imagen"
-					description="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-					button={button} />
+					<h3 className="d-flex justify-content-center fw-bold mt-4">Otros usuarios han seleccionado estos regalos como favorito...</h3>
+					<SwiperSlide>
+					<Card
+						imageUrl={imageUrl1}
+						title="Título imagen"
+						description="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+						button={button} />
+					</SwiperSlide>
 
-				<Card
-					imageUrl={imageUrl1}
-					title="Título imagen"
-					description="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-					button={button} />
+					<SwiperSlide>
+					<Card
+						imageUrl={imageUrl1}
+						title="Título imagen"
+						description="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+						button={button} />
+					</SwiperSlide>
+
+					<SwiperSlide>
+					<Card
+						imageUrl={imageUrl1}
+						title="Título imagen"
+						description="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+						button={button} />
+					</SwiperSlide>
+
+					<SwiperSlide>
+					<Card
+						imageUrl={imageUrl1}
+						title="Título imagen"
+						description="is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+						button={button} />
+					</SwiperSlide>
 
 
+				</div>
 
-			</div>
-
-
+			</Swiper>
 		</div>
 
 
